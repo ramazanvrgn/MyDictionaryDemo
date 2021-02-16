70f4fbe31b187ef9cf6ae10afa44b0eb8a8842f2
ramazanvrgn <79042297+ramazanvrgn@users.noreply.github.com>
2021-02-16 23:05:38 +0300

    Add files via upload

---
+++ MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] _key;
+        TValue[] _value;
+        TKey[] _tempKey;
+        TValue[] _tempValue;
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+      
+        public void Add(TKey key , TValue value)
+        {
+            _tempKey = _key;
+            _tempValue = _value;
+
+            _key = new TKey[_key.Length + 1];
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                 _key[i] =_tempKey[i];
+                 _value[i]=_tempValue[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+
+        }
+        public void Listed()
+        {
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine("No: {0} Adı Soyadı: {1} ", _key[i], _value[i]);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenci = new MyDictionary<int, string>() ;
+            ogrenci.Add(322,"Leyla Başar");
+            ogrenci.Add(323, "Aslan Ballı");
+            ogrenci.Add(326, "Ekrem Yalın");     
+            
+            ogrenci.Listed();
+
+        }
+    }
+}
